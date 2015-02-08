586616895075e866702486f8d535ae3df0c3cd8e
magickreeator <magickreeator@gmail.com>
2015-02-08 20:30:06 +0300

    it works for large texts

---
+++ txt-lang-detector/Func.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace txt_lang_detector
+{
+    class Func
+    {
+        // Если текущий элемент больше тех, что расположены ниже, то нужно вернуть истину.
+        bool check_down(double[] count, int i)
+        {
+            for (int j = i; j < count.Length - 1; j++)
+            {
+
+                if (count[i] > count[j + 1])
+                {
+
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        // Если текущий элемент меньше тех, что расположены выше, то нужно вернуть истину.
+        bool check_up(double[] count, int i)
+        {
+            for (int j = i; j > 1; j--)
+            {
+
+                if (count[i] < count[j - 1])
+                {
+
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public int detect(char[] lang, string text)
+        {
+            // Для  хранения частот.
+            double[] Freq = new double[lang.Length];
+            // 
+            bool[] rank_check = new bool[lang.Length];
+            // Для хранения количества букв.
+            double letter_count = 0;
+            // Обнуление значений элементов массива.
+            for (int i = 0; i < lang.Length; i++)
+            {
+                Freq[i] = 0;
+            }
+            // 
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < lang.Length; j++)
+                {
+                    // Если рассматриваемый символ является буквой.
+                    if (text[i] >= 'a' && text[i] <= 'z' || text[i] >= 'а' && text[i] <= 'я')
+                    {
+                        letter_count++;
+                        if (text[i] == lang[j])
+                        {
+                            //char[] ccounter = new char[lang.Length];
+                            // Инкрементировать количество найденных букв в позиции предпологаемых частот.
+                            Freq[j]++;
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < lang.Length; i++)
+            {
+                // Вычисление частот по формуле Freq_x=Q_x/Q_all.
+                Freq[i] = Freq[i] / letter_count;
+            }
+            // Для хранения совпадений найденных частот и эталонных частот.
+            int equals = 0;
+            for (int i = 0; i < lang.Length; i++)
+            {
+                if (check_down(Freq, i))
+                {
+                    equals++;
+                }
+
+            }
+            if (check_up(Freq, lang.Length - 1))
+            {
+                equals++;
+            }
+            // Если количество совпадений больше хотя бы половины количества букв в языке, то вернуть истину.
+
+            return equals;
+
+        }
+    }
+}
--- txt-lang-detector/mainForm.cs
+++ txt-lang-detector/mainForm.cs
@@ -12,9 +12,46 @@ namespace txt_lang_detector
 {
     public partial class mainForm : Form
     {
+        Func Functions = new Func();
+        // Частотность букв русского языка ( https://ru.wikipedia.org/wiki/%D0%A7%D0%B0%D1%81%D1%82%D0%BE%D1%82%D0%BD%D0%BE%D1%81%D1%82%D1%8C )
+        char[] ru = { 'о', 'е', 'a', 'и', 'н', 'т', 'с', 'р', 'в', 'л', 'к', 'м', 'д', 'п', 'у', 'я', 'ы', 'ь', 'г', 'з', 'б', 'ч', 'й', 'х', 'ж', 'ш', 'ю', 'ц', 'щ', 'э', 'ф', 'ъ', 'ё' };
+        // Частотность букв английского языка ( https://en.wikipedia.org/wiki/Letter_frequency )
+        char[] en = { 'e', 't', 'a', 'o', 'i', 'n', 's', 'h', 'r', 'd', 'l', 'c', 'u', 'm', 'w', 'f', 'g', 'y', 'p', 'b', 'v', 'k', 'j', 'x', 'q', 'z' };
+        // Частотность букв украинского языка ( http://megamozg.ru/post/2336/ )
+        char[] ua = { 'о', 'а', 'н', 'и', 'т', 'е', 'в', 'і', 'р', 'с', 'к', 'л', 'у', 'д', 'м', 'п', 'я', 'з', 'ь', 'б', 'г', 'ч', 'й', 'х', 'ж', 'ю', 'ш', 'ц', 'щ', 'є', 'ї', 'ф', '\'', 'ґ' };
+        int rus, eng, ukr;
         public mainForm()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                rus = Functions.detect(ru, textBox1.Text.ToLower());
+                eng = Functions.detect(en, textBox1.Text.ToLower());
+                ukr = Functions.detect(ua, textBox1.Text.ToLower());
+
+                if (rus > eng && rus > ukr)
+                {
+                    langNameLabel.Text = "Язык текста: русский";
+                }
+                else if (eng > rus && eng > ukr)
+                {
+                    langNameLabel.Text = "Язык текста: английский";
+                }
+                else if (ukr > rus && ukr > eng)
+                {
+                    langNameLabel.Text = "Язык текста: украинский";
+                }
+                else { langNameLabel.Text = "Язык текста: не удалось определить язык"; }
+            }
+            else
+            {
+                langNameLabel.Text = "Язык текста: ";
+            }
+
         }
     }
 }
